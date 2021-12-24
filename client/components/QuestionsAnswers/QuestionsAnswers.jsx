0c5c9e2872cf1e6e7632c852ec0686cbc4ba184f
Jinyan Li <jinyanli@DESKTOP-G24AK6R.localdomain>
2021-12-23 19:58:57 -0500

    building quest-list

--- client/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ client/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -1,13 +1,65 @@
 import React from 'react';
-import { COLORS } from '../../settings/colors';
 import styled from 'styled-components';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
 
-const QuestionsAnswers = () => (
-  <Header>Q&A Module</Header>
-);
+
+class QuestionsAnswers extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/qa/questions')
+      .then((data) => {
+        this.setState({
+          questions: data.results
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        <h1>Questions & Answers</h1>
+        <input type='text' placeholder='Enter your question here'></input>
+        <button><FontAwesomeIcon icon={faSearch} /></button>
+        <div>
+          {this.state.questions.map((question, i) => {
+            return (
+              <div key={i} size='4'>
+                <div>{question.question_body}</div>
+                <div>Helpful?<a href="#">Yes</a>(1){question.results.question_helpfuless}</div>
+              </div>
+            )
+          })}
+        </div>
+
+
+      </div>
+    )
+
+  }
+
+}
+
+
+
 
 const Header = styled.h1`
-  color: ${COLORS.button};
+  color: ${props => props.theme.navbarText};
 `;
 
 export default QuestionsAnswers;
