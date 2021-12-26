608782b9578511a3781719d672b4d48bfd878118
Jinyan Li <jinyanli@DESKTOP-G24AK6R.localdomain>
2021-12-25 23:39:26 -0500

    questions list and answers list display

--- client/components/QuestionsAnswers/QuestionsAnswers.jsx
+++ client/components/QuestionsAnswers/QuestionsAnswers.jsx
@@ -5,6 +5,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 import api from '../../api.js';
 
+
+// const AnswerTitle = styled.div`
+//   display: inline-block
+// `
+
+// const AnswerBody = styled.div`
+//   display: inline-block
+// `
+
+const Wrapper = styled.div`
+  display: flex
+
+`;
+
+const Photos = styled.div`
+  float: left
+`;
+
+const ByUser = styled.div`
+  clear: both
+`;
+
 const QuestionsHidden = styled.div`
   display: none
 `;
@@ -19,7 +41,7 @@ const AlignRight = styled.span`
 
 const YesLink = styled.u`
 
-`
+`;
 
 
 
@@ -70,45 +92,152 @@ class QuestionsAnswers extends React.Component {
 
   render() {
     let {questions} = this.state;
-    var questionsShow = this.state.questions.slice(0, 4).map((question) => {
+    var months = {
+      '01': 'Jan',
+      '02': 'Feb',
+      '03': 'Mar',
+      '04': 'Apr',
+      '05': 'May',
+      '06': 'Jun',
+      '07': 'Jul',
+      '08': 'Aug',
+      '09': 'Sep',
+      '10': 'Oct',
+      '11': 'Nov',
+      '12': 'Dec'
+    };
 
+    var questionsShow = this.state.questions.slice(0, 4).map((question) => {
       const answers = Object.values(question.answers);
+
+      const answersShow = answers.slice(0, 2).map((answer) => {
+        let timeArr = answer.date.split('T')[0].split('-');
+        return (
+          <div key={answer.id}>
+            <div>{answer.body}</div>
+            <div>
+              {answer.photos.map((photo, index) => {
+                return (
+                  <Photos key={index}>
+                    <img src={photo.index} alt='photo'/>
+                  </Photos>
+                );
+              })}
+            </div>
+            <ByUser>
+              <span>by {answer.answerer_name}, {months[timeArr[1]]} {timeArr[2]}, {timeArr[0]}&emsp;|&emsp;</span>
+              <span>Helpful?{question.question_helpfuless} <YesLink onClick={this.addHelpful}>Yes</YesLink>({question.question_helpfulness})&emsp;|&emsp;</span>
+              <span><YesLink onClick={this.addHelpful}>Report</YesLink></span>
+            </ByUser>
+          </div>
+        );
+      });
+
+      const answersHidden = answers.slice(2, 100).map((answer) => {
+        let timeArr = answer.date.split('T')[0].split('-');
+        return (
+          <div key={answer.id}>
+            <div>{answer.body}</div>
+            <div>
+              {answer.photos.map((photo, index) => {
+                return (
+                  <Photos key={index}>
+                    <img src={photo.index} alt='photo'/>
+                  </Photos>
+                );
+              })}
+            </div>
+            <ByUser>
+              <span>by {answer.answerer_name}, {months[timeArr[1]]} {timeArr[2]}, {timeArr[0]}&emsp;|&emsp;</span>
+              <span>Helpful?{question.question_helpfuless} <YesLink onClick={this.addHelpful}>Yes</YesLink>({question.question_helpfulness})&emsp;|&emsp;</span>
+              <span><YesLink onClick={this.addHelpful}>Report</YesLink></span>
+            </ByUser>
+          </div>
+        );
+      });
 
       return (
         <div key={question.question_id}>
           <div>Q: {question.question_body}
             <AlignRight>Helpful?{question.question_helpfuless} <YesLink onClick={this.addHelpful}>Yes</YesLink>({question.question_helpfulness})</AlignRight>
           </div>
-          <div>
-            {answers.slice(0, 2).map((answer) => {
-              return (
-                <div key={answer.id}>A: {answer.body}</div>
-              )
-            })}
-          </div>
-          <AnswersHidden>
-            {answers.slice(2, 100).map((answer) => {
-              return (
-                <div key={answer.id}>A: {answer.body}</div>
-              )
-            })}
-          </AnswersHidden>
-
-
+          <Wrapper>
+            <div>A:</div>
+            <div>
+              {answersShow}
+              <AnswersHidden>
+                {answersHidden}
+              </AnswersHidden>
+            </div>
+          </Wrapper>
         </div>
       );
-    })
+    });
 
     var questionsHidden = this.state.questions.slice(4, 100).map((question) => {
+      const answers = Object.values(question.answers);
+
+
       return (
-        <QuestionsHidden key={question.question_id} >
-          <div>
-            <div>Q:{question.question_body} <span >Helpful?{question.question_helpfuless}</span></div>
+        <QuestionsHidden key={question.question_id}>
+          <div>Q: {question.question_body}
+            <AlignRight>Helpful?{question.question_helpfuless} <YesLink onClick={this.addHelpful}>Yes</YesLink>({question.question_helpfulness})</AlignRight>
           </div>
+
+          <Wrapper>
+            <div>A:</div>
+            <div>
+              {answers.slice(0, 2).map((answer) => {
+                let timeArr = answer.date.split('T')[0].split('-');
+                return (
+                  <div key={answer.id}>
+                    <div>{answer.body}</div>
+                    <div>
+                      {answer.photos.map((photo, index) => {
+                        return (
+                          <Photos key={index}>
+                            <img src={photo.index} alt='photo'/>
+                          </Photos>
+                        );
+                      })}
+                    </div>
+                    <ByUser>
+                      <span>by {answer.answerer_name}, {months[timeArr[1]]} {timeArr[2]}, {timeArr[0]}&emsp;|&emsp;</span>
+                      <span>Helpful?{question.question_helpfuless} <YesLink onClick={this.addHelpful}>Yes</YesLink>({question.question_helpfulness})&emsp;|&emsp;</span>
+                      <span><YesLink onClick={this.addHelpful}>Report</YesLink></span>
+                    </ByUser>
+                  </div>
+                );
+              })}
+              <AnswersHidden>
+                {answers.slice(2, 100).map((answer) => {
+                  let timeArr = answer.date.split('T')[0].split('-');
+                  return (
+                    <div key={answer.id}>
+                      <div>{answer.body}</div>
+                      <div>
+                        {answer.photos.map((photo, index) => {
+                          return (
+                            <Photos key={index}>
+                              <img src={photo.index} alt='photo'/>
+                            </Photos>
+                          );
+                        })}
+                      </div>
+                      <ByUser>
+                        <span>by {answer.answerer_name}, {months[timeArr[1]]} {timeArr[2]}, {timeArr[0]}&emsp;|&emsp;</span>
+                        <span>Helpful?{question.question_helpfuless} <YesLink onClick={this.addHelpful}>Yes</YesLink>({question.question_helpfulness})&emsp;|&emsp;</span>
+                        <span><YesLink onClick={this.addHelpful}>Report</YesLink></span>
+                      </ByUser>
+                    </div>
+                  );
+                })}
+              </AnswersHidden>
+            </div>
+          </Wrapper>
         </QuestionsHidden>
       );
-    })
-
+    });
 
     return (
       <div>
@@ -121,13 +250,8 @@ class QuestionsAnswers extends React.Component {
         </div>
       </div>
     );
-
   }
-
 }
-
-
-
 
 const Header = styled.h1`
   color: ${props => props.theme.navbarText};
