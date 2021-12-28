03617eecffa27c61e13116cb6f6567f04dcf785f
Alex Sieke <asieke@gmail.com>
2021-12-28 16:28:03 -0500

    finished add a review component

--- client/components/RatingsReviews/RatingsReviews.jsx
+++ client/components/RatingsReviews/RatingsReviews.jsx
@@ -16,34 +16,55 @@ class RatingsReviews extends Component {
       reviews: null,
       reviewsShowing: 2,
       modalShowing: false,
-      filters: { ...allTrue }
+      filters: { ...allTrue },
     };
+  }
+
+  fetch(id) {
+    api.getReviews({ product_id: id, count: 100 }).then((res) => {
+      this.setState({ reviews: res.results });
+    });
+    api.getReviewMeta({ product_id: id }).then((res) => {
+      this.setState({ reviewMeta: res });
+    });
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     const { id } = this.props.product;
     if (id && JSON.stringify(prevProps) !== JSON.stringify(this.props)) {
-      api.getReviews({ product_id: id, count: 100 }).then((res) => {
-        this.setState({ reviews: res.results });
-      });
-      api.getReviewMeta({ product_id: id }).then((res) => {
-        this.setState({ reviewMeta: res });
-      });
+      this.fetch(id);
     }
   }
 
   //only show star ratings with X stars
   updateFilter(stars) {
     let temp = getRatingFilters(this.state.filters, stars);
-    this.setState({ filters: temp }, () => console.log(Object.values(this.state.filters)));
+    this.setState({ filters: temp }, () =>
+      console.log(Object.values(this.state.filters))
+    );
+  }
+
+  submitReview(data) {
+    this.setState({ modalShowing: false });
+    this.fetch(data.product_id);
   }
 
   render() {
-    const { reviewMeta, reviews, reviewsShowing, modalShowing, filters } = this.state;
+    const { reviewMeta, reviews, reviewsShowing, modalShowing, filters } =
+      this.state;
+
+    const { product } = this.props;
 
     return (
       <Container>
-        {modalShowing === true && <AddReview onClose={() => this.setState({ modalShowing: false })} />}
+        {modalShowing === true && reviewMeta && (
+          <AddReview
+            onClose={() => this.setState({ modalShowing: false })}
+            onSubmit={(data) => this.submitReview(data)}
+            meta={reviewMeta}
+            product={product}
+          />
+        )}
         <h3>Ratings and Reviews</h3>
         <MainContainer>
           <LeftContainer>
@@ -62,13 +83,15 @@ class RatingsReviews extends Component {
                 reviews={reviews}
                 reviewsShowing={reviewsShowing}
                 filters={filters}
-                showMore={() => this.setState({ reviewsShowing: reviewsShowing + 2 })}
+                showMore={() =>
+                  this.setState({ reviewsShowing: reviewsShowing + 2 })
+                }
                 addReview={() => this.setState({ modalShowing: true })}
               />
             )}
           </RightContainer>
         </MainContainer>
-      </Container >
+      </Container>
     );
   }
 }
